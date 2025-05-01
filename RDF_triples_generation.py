01de93c0638f0030a5c900e8923d69301c31a87d
Shafi_2065 <33521064+Shafi2065@users.noreply.github.com>
2025-05-01 19:58:36 +0100

    RDF generations task
    - Added ontology file
    - Added an RDF generated data file
    - RDF generate triples from dataset functional file
    - RDF SPARQL queries file

---
+++ RDF_triples_generation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from rdflib import Graph, Literal, RDF, Namespace, XSD
+
+df = pd.read_csv("C:/Users/Shafi/Documents/GitHub/R-S_Semantics/Ontologies & Dataset/RS_Dataset.csv")
+
+g = Graph()
+shafi = Namespace("http://www.city.ac.uk/inm713-in3067/2025/Shafi/")
+g.bind("shafi", shafi)
+
+for index, row in df.iterrows():
+    user_id = f"User_{row['User ID']}"
+    workout_id = f"Workout_{row['User ID']}"
+    hr_id = f"HeartRate_{row['User ID']}"
+    nutrition_id = f"NutritionPlan_{row['User ID']}"
+    dailycal_id = f"DailyCalories_{row['User ID']}"
+
+    user_uri = shafi[user_id]
+    workout_uri = shafi[workout_id]
+    hr_uri = shafi[hr_id]
+    nutrition_uri = shafi[nutrition_id]
+    dailycal_uri = shafi[dailycal_id]
+
+    # Define types
+    g.add((user_uri, RDF.type, shafi.User))
+    g.add((workout_uri, RDF.type, shafi.Workout))
+    g.add((hr_uri, RDF.type, shafi.HeartRate))
+    g.add((nutrition_uri, RDF.type, shafi.NutritionPlan))
+
+    # Link user to other entities
+    g.add((user_uri, shafi.hasWorkout, workout_uri))
+    g.add((user_uri, shafi.hasHealthMetric, hr_uri))
+    g.add((user_uri, shafi.hasNutritionPlan, nutrition_uri))
+
+    # data properties 
+    g.add((user_uri, shafi.age, Literal(row['Age'], datatype=XSD.integer)))
+    g.add((workout_uri, shafi.caloriesBurned, Literal(row['Calories Burned'], datatype=XSD.float)))
+    g.add((hr_uri, shafi.heartRate, Literal(row['Heart Rate (bpm)'], datatype=XSD.integer)))
+    g.add((nutrition_uri, shafi.waterIntake, Literal(row['Water Intake (liters)'], datatype=XSD.float)))
+    g.add((nutrition_uri, shafi.dailyCaloriesIntake, Literal(row['Daily Calories Intake'], datatype=XSD.float)))
+    g.add((nutrition_uri, shafi.bodyFat, Literal(row['Body Fat (%)'], datatype=XSD.float)))
+
+g.serialize(destination="rdf_generated_data.ttl", format="turtle")
+print("RDF data generated and saved to rdf_generated_data.ttl")
---
+++ sparql_query.py
@@ -0,0 +1,72 @@
+from rdflib import Graph, RDF, RDFS
+
+
+# Create a Graph and parse TTL files
+g = Graph()
+g.parse(r"C:\Users\Shafi\Documents\GitHub\R-S_Semantics\Ontologies & Dataset\RDF_generated_data.ttl", format="ttl")
+g.parse(r"C:\Users\Shafi\Documents\GitHub\R-S_Semantics\Ontologies & Dataset\rdf3_googlekg.ttl", format="ttl")
+g.parse(r"C:\Users\Shafi\Documents\GitHub\R-S_Semantics\Ontologies & Dataset\rdf3_wikidata.ttl", format="ttl")
+
+for s, p, o in g.triples((None, RDF.type, None)):
+    if str(o).startswith("https://schema.org"):
+        print(s, p, o)
+
+# SPARQL queries
+q = """
+PREFIX shafi: <http://www.city.ac.uk/inm713-in3067/2025/Shafi/>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX schema: <https://schema.org/>
+
+# Query 1: Get all users and their workouts
+# SELECT ?user ?workout
+# WHERE {
+#   ?user a shafi:User ;
+#         shafi:hasWorkout ?workout .
+# }
+
+
+# Query 2: Get all workouts with calories burned greater than 500
+# SELECT ?workout ?calories
+# WHERE {
+#   ?workout a shafi:Workout ;
+#            shafi:caloriesBurned ?calories .
+#   FILTER (?calories > 500)
+# }
+
+# Query 3: Get all users with water intake less than 2.0 liters
+# SELECT ?user ?water
+# WHERE {
+#   ?user a shafi:User ;
+#         shafi:waterIntake ?water .
+#   FILTER (?water < 2.0)
+# }
+
+# Query 4: Get all workouts that use specific equipment
+
+# SELECT ?workout ?equipment
+# WHERE {
+#   ?workout a shafi:Workout ;
+#            shafi:performsExerciseWith ?equipment .
+# }
+
+# Query 5: Get all users and their activities using Wikidata
+# SELECT ?user ?activity
+# WHERE {
+#   ?user shafi:doesActivity ?activity .
+# }
+
+# Query 6: Get all users and their activities using Google Knowledge Graph
+SELECT ?person ?role
+WHERE {
+  ?person a ?role .
+  FILTER(STRSTARTS(STR(?role), "https://schema.org/"))
+}
+"""
+
+
+# Run the query
+results = g.query(q)
+
+# Print results
+for row in results:
+    print(f":Person {row.person}, :Role {row.role}")
